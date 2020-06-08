edda019f27b6b859133e4d357b6a1e95cda9d6cf
gmlwn7245 <gmlwn7245@naver.com>
2020-06-08 17:06:04 +0900

    todolist-1

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h1>TODO it!</h1>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+    <link rel="shortcut icon" href="src/assets/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="src/assets/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin : 2.5rem 0 1.5rem;
+}
+</style>
